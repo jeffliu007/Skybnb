c43ba5ec079fd60a4290f50d89b16848ba52e897
Jeff L <jeffreyliu97@gmail.com>
2022-12-15 14:49:52 -0800

    Add user logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,4 +25,9 @@ router.post("/", async (req, res, next) => {
   });
 });
 
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
 module.exports = router;
