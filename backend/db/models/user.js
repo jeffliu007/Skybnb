f4530aff79cbb69c714bf5005ec6d17036b90006
Jeff L <jeffreyliu97@gmail.com>
2022-12-20 11:08:40 -0800

    added fname lname to get curr user response

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,8 +5,8 @@ const bcrypt = require("bcryptjs");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, username, email } = this; // context will be the User instance
-      return { id, username, email };
+      const { id, username, email, firstName, lastName } = this; // context will be the User instance
+      return { id, username, email, firstName, lastName };
     }
 
     validatePassword(password) {
