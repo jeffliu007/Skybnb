d0e39878c72efe9a715b078b5192e15a41bc9dfa
Jeff L <jeffreyliu97@gmail.com>
2022-12-20 23:07:10 -0800

    created up to getting spots by specific id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,11 +5,19 @@ const {
   requireAuth,
 } = require("../../utils/auth");
 
-const { Spot, SpotImage, Review, sequelize } = require("../../db/models");
+const {
+  Spot,
+  SpotImage,
+  Review,
+  User,
+  Booking,
+  sequelize,
+} = require("../../db/models");
 
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const { TimeoutError } = require("sequelize");
+const user = require("../../db/models/user");
 
 const router = express.Router();
 
@@ -72,7 +80,7 @@ router.get("/current", requireAuth, async (req, res) => {
       attributes: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]],
     });
     spot = spot.toJSON();
-    spot.getRating = review[0].dataValues.avgRating;
+    spot.avgRating = review[0].dataValues.avgRating;
     const previewImage = await SpotImage.findOne({
       where: {
         preview: true,
@@ -93,28 +101,105 @@ router.get("/current", requireAuth, async (req, res) => {
   });
 });
 
-//get specific spot by id not finished
+//get specific spot by id
 
-router.get("/:spotId", async (req, res) => {
+router.get("/:spotId", async (req, res, next) => {
   const id = req.params.spotId;
-  const specificSpot = await Spot.findOne({
+
+  const spot = await Spot.findOne({
     where: {
-      id: id,
+      id,
     },
     include: [
       {
         model: Review,
-        attributes: [],
-        required: true,
+      },
+      {
+        model: User,
+        attributes: {
+          exclude: [
+            "username",
+            "email",
+            "hashedPassword",
+            "createdAt",
+            "updatedAt",
+          ],
+        },
       },
       {
         model: SpotImage,
-        attributes: [],
-        required: true,
+        attributes: {
+          exclude: ["spotId", "createdAt", "updatedAt"],
+        },
       },
     ],
   });
-  return res.json(specificSpot);
+
+  if (!spot) {
+    const err = new Error("Spot couldn't be found");
+    err.title = "Invalid spot";
+    err.status = 404;
+    return next(err);
+  }
+
+  const avgRev = await Review.findAll({
+    attributes: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]],
+  });
+
+  const specificSpot = spot.toJSON();
+
+  specificSpot.avgStarRating = avgRev[0].dataValues.avgRating;
+  specificSpot.numReviews = specificSpot.Reviews.length;
+  specificSpot.Owner = specificSpot.User;
+  //remove the tables we edited
+  delete specificSpot.User;
+  delete specificSpot.Reviews;
+
+  res.json(specificSpot);
+  // const reviewNum = await Review.count({
+  //   where: {
+  //     spotId: id,
+  //   },
+  // });
+  // // console.log(reviewNum);
+
+  // const specificSpot = await Spot.findOne({
+  //   where: {
+  //     id: id,
+  //   },
+  //   attributes: [
+  //     "id",
+  //     "ownerId",
+  //     "address",
+  //     "city",
+  //     "state",
+  //     "country",
+  //     "lat",
+  //     "lng",
+  //     "name",
+  //     "description",
+  //     "price",
+  //     "createdAt",
+  //     "updatedAt",
+  //     [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"],
+  //     [sequelize.col("url"), "previewImage"],
+  //   ],
+  //   include: [
+  //     {
+  //       model: Review,
+  //       attributes: [],
+  //       required: true,
+  //     },
+  //     {
+  //       model: SpotImage,
+  //       attributes: [],
+  //       required: true,
+  //     },
+  //   ],
+  // });
+  // return res.json(specificSpot);
 });
+
+//continue on creating a spot tomorrow
 
 module.exports = router;
