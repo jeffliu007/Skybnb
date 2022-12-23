e61964e3e298ffdabd3e63a5e2a71d998d5d8b40
Jeff L <jeffreyliu97@gmail.com>
2022-12-22 23:39:27 -0800

    delete review done

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -24,6 +24,8 @@ const { Op } = require("sequelize");
 
 const router = express.Router();
 
+//get current users reviews
+
 router.get("/current", requireAuth, async (req, res) => {
   const { user } = req;
 
@@ -184,5 +186,35 @@ router.put(
     return res.json(revId);
   }
 );
+
+//delete a review
+
+router.delete("/:reviewId", requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const id = req.params.reviewId;
+
+  const revId = await Review.findByPk(id);
+
+  if (!revId) {
+    const err = new Error("Review couldn't be found");
+    err.status = 404;
+    err.message = "Review couldn't be found";
+    return next(err);
+  }
+
+  if (user.id !== revId.userId) {
+    const err = new Error("Unauthorized user");
+    err.status = 404;
+    err.message = "Unauthorized user";
+    return next(err);
+  }
+
+  revId.destroy();
+
+  return res.json({
+    message: "Successfully deleted",
+    statusCode: res.statusCode,
+  });
+});
 
 module.exports = router;
