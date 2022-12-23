7fd1989ce5124f9a83e30f84cbe0ff340352f63b
Jeff L <jeffreyliu97@gmail.com>
2022-12-23 15:25:36 -0800

    revimg delete added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,8 @@ const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewsRouter = require("./reviews.js");
 const bookingsRouter = require("./bookings.js");
-const spotimagesRouter = require("./spot-images.js");
+const spotImagesRouter = require("./spot-images.js");
+const reviewImagesRouter = require("./review-images.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -22,7 +23,9 @@ router.use("/reviews", reviewsRouter);
 
 router.use("/bookings", bookingsRouter);
 
-router.use("/spot-images", spotimagesRouter);
+router.use("/spot-images", spotImagesRouter);
+
+router.use("/review-images", reviewImagesRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
