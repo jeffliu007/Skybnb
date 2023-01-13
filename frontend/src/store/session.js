f68907fe3efdeb13532d0dccaaa4c2a006aa7240
Jeff L <jeffreyliu97@gmail.com>
2023-01-13 12:42:12 -0800

    finished init signup action func

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,8 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+//actions start
+
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -16,6 +18,9 @@ const removeUser = () => {
   };
 };
 
+//thunks here
+
+//
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
@@ -36,6 +41,27 @@ export const restoreUser = () => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
+
+export const signupUser = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+
+  dispatch(setUser(data.user));
+
+  return response;
+};
+
+//main reducers start here
 
 const initialState = { user: null };
 
