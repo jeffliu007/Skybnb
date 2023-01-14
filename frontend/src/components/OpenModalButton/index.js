a2ed60ec3b2507585675ec2821585552b9a9603e
Jeff L <jeffreyliu97@gmail.com>
2023-01-13 17:11:52 -0800

    finished signup and login modal

---
+++ frontend/src/components/OpenModalButton/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useModal } from "../../context/Modal";
+
+function OpenModalButton({
+  modalComponent, // component to render inside the modal
+  buttonText, // text of the button that opens the modal
+  onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
+  onModalClose, // optional: callback function that will be called once the modal is closed
+}) {
+  const { setModalContent, setOnModalClose } = useModal();
+
+  const onClick = () => {
+    if (typeof onButtonClick === "function") onButtonClick();
+    if (typeof onModalClose === "function") setOnModalClose(onModalClose);
+    setModalContent(modalComponent);
+  };
+
+  return <button onClick={onClick}>{buttonText}</button>;
+}
+
+//notes
+
+// Now, let's say you wanted to print "Greeting completed" in the console logs whenever the user closes the "Hello World!" modal. You could add the following callback function as the onModalClose prop to the OpenModalButton component:
+
+// const Greeting = () => {
+//   return (
+//     <OpenModalButton
+//       buttonText="Greeting"
+//       modalComponent={<h2>Hello World!</h2>}
+//       onButtonClick={() => console.log("Greeting initiated")}
+//       onModalClose={() => console.log("Greeting completed")}
+//     />
+//   );
+// };
+
+export default OpenModalButton;
