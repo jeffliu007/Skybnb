fc4b7e3725ad54a816797ce79eb84686c8e9c972
Jeff L <jeffreyliu97@gmail.com>
2023-03-23 11:03:42 -0700

    booking spot polished

--- frontend/src/components/BookingsAll/UserBookingsCard.js
+++ frontend/src/components/BookingsAll/UserBookingsCard.js
@@ -1,8 +1,25 @@
 import { convertDate } from "./helperDates";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const UserBookingsCard = ({ book, spotId, userId }) => {
+  const history = useHistory();
+  const bookings = useSelector((state) => state.bookings.user);
+
+  const handleBookingClick = () => {
+    history.push(`/spots/${book.spotId}`);
+  };
+
+  if (!bookings)
+    return (
+      <div className="BookingsPage-Container">
+        <h1>No Upcoming Trips Available</h1>
+        <h2>Time to pack your bags and book a relaxing vacation!</h2>
+      </div>
+    );
+
   return (
-    <div className="BookingsPage-Card-Holder">
+    <div className="BookingsPage-Card-Holder" onClick={handleBookingClick}>
       <div className="BookingsPage-Card-Left">
         <div className="BookingsPage-Card-Img">
           <img src={book.Spot.SpotImages[0].url} />
