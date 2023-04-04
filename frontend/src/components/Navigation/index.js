62b107aa0899411fe0f984ecbe6db764f3e1f92a
Jeff L <jeffreyliu97@gmail.com>
2023-04-03 17:31:26 -0700

    changed import

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import AddNewSpotModal from "../CreateSpotModal";
-import Searchbar from "../SearchSpots";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
